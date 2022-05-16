3d0ae486769a6fae80664692e31284abdd52ee2c
Ismail <adegbiteismail27@gmail.com>
2022-05-15 21:07:34 -1100

    initiate design

--- src/pages/CountryDetail.js
+++ src/pages/CountryDetail.js
@@ -1,13 +1,28 @@
 import { useParams } from "react-router-dom"
 import Header from "../components/Header"
-const CountryDetail = () => {
+const CountryDetail = ({data, darkMode, toggleDarkMode}) => {
     const {countryName} = useParams()
+    const country = data.find(item => item.name === countryName)
     return (
         
-        <>
-            <Header/>
-            Hello {countryName}
-        </>
+        <section className={`bbg-white ${darkMode?'bbg-dark': ''}`}>
+             <Header
+                darkMode={darkMode}
+                toggleDarkMode={toggleDarkMode}
+            />
+            <main>
+                <div className="country-flag">
+                <img src={country.flag} alt="country"/>
+                </div>
+                <div className="country-details">
+                    <div className="main-details">
+                        <div className="d1"></div>
+                        <div className="d2"></div>
+                    </div>
+                    <div className="border-countries"></div>
+                </div>
+            </main>
+        </section>
     )
 }
 
